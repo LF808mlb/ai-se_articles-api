340fcaac2c41e762b1d3813422d5d887732cac6c
lesley <sprite_lksln31@yahoo.com>
2026-05-30 12:53:16 -1000

    git commit -m "Extracting text from a PDF (controllers/documents.ts)"

--- src/controllers/documents.ts
+++ src/controllers/documents.ts
@@ -1,5 +1,21 @@
 import type { Request, Response } from 'express';
+import { readFileSync } from 'fs';
+import { PDFParse } from 'pdf-parse';
 
-export const uploadDocument = async (_req: Request, _res: Response) => {};
+export const uploadDocument = async (req: Request, res: Response) => {
+    if (!req.file) {
+    // File not found
+    return;
+  }
+  
+  // Obtain a buffer with the file, and pass it to the parser
+  const buffer = readFileSync(req.file.path);
+  const parser = new PDFParse({ data: buffer });
+  
+  // Parse the text
+  const { text } = await parser.getText();
+  
+ 
+};
 
 export const getDocuments = async (_req: Request, _res: Response) => {};
